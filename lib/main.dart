ec9b07c951be4bb4b2852c9fe31f80f208584955
Rene Balke <renebalke2@gmail.com>
2019-01-09 16:33:14 +0100

    added ic_launcher

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Monkey Game',
       debugShowCheckedModeBanner: false,
       home: Start(),
       routes: <String, WidgetBuilder>{
